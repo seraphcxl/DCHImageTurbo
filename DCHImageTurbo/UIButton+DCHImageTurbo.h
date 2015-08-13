06ae419db6896b75e5a4101dd5f15c423c1d9e41
Derek Chen <seraph.cxl@gmail.com>
2015-08-13 11:10:05 +0800

    update for UIButton

--- DCHImageTurbo/UIButton+DCHImageTurbo.h
+++ DCHImageTurbo/UIButton+DCHImageTurbo.h
@@ -7,20 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <SDWebImage/UIView+WebCacheOperation.h>
 
-//extern NSString * const key_DCHImageTurbo_UIButton_WebImageURL;
-//extern NSString * const key_DCHImageTurbo_UIButton_WebHighlightedImageURL;
-//extern NSString * const key_DCHImageTurbo_UIButton_LocalImagePath;
-//extern NSString * const key_DCHImageTurbo_UIButton_LocalHighlightedImagePath;
-//
-//extern NSString * const key_DCHImageTurbo_UIButton_WebImageLoadOperation;
-//extern NSString * const key_DCHImageTurbo_UIButton_WebHighlightedImageLoadOperation;
-//extern NSString * const key_DCHImageTurbo_UIButton_LocalImageLoadOperation;
-//extern NSString * const key_DCHImageTurbo_UIButton_LocalHighlightedImageLoadOperation;
+extern NSString * const key_DCHImageTurbo_UIButton_WebImageURLStorage;
+extern NSString * const key_DCHImageTurbo_UIButton_WebBackgroundImageURLStorage;
+extern NSString * const key_DCHImageTurbo_UIButton_LocalImagePathStorage;
+extern NSString * const key_DCHImageTurbo_UIButton_LocalBackgroundImagePathStorage;
+
+extern NSString * const key_DCHImageTurbo_UIButton_WebImageLoadOperation;
+extern NSString * const key_DCHImageTurbo_UIButton_WebBackgroundImageLoadOperation;
+extern NSString * const key_DCHImageTurbo_UIButton_LocalImageLoadOperation;
+extern NSString * const key_DCHImageTurbo_UIButton_LocalBackgroundImageLoadOperation;
 
 @interface UIButton (DCHImageTurbo)
 
-//#pragma mark - Web image
+- (NSString *)dch_createKeyForButtonLoadImageWithPrefix:(NSString *)prefix andState:(UIControlState)state;
+
+#pragma mark - Web image
+- (NSMutableDictionary *)dch_webImageURLStorage;
+- (NSURL *)dch_currentWebImageURL;
+- (NSURL *)dch_webImageURLForState:(UIControlState)state;
+
+- (NSMutableDictionary *)dch_webBackgroundImageURLStorage;
+- (NSURL *)dch_currentWebBackgroundImageURL;
+- (NSURL *)dch_webBackgroundImageURLForState:(UIControlState)state;
+
+- (void)dch_setWebImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state;
+- (void)dch_cancelWebImageURLForState:(UIControlState)state;
+
+- (void)dch_setWebBackgroundImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state;
+- (void)dch_cancelWebBackgroundImageURLForState:(UIControlState)state;
 //- (NSURL *)dch_currentWebImageURL;
 //- (void)dch_setWebImageURL:(NSURL *)url;
 //
@@ -34,7 +50,20 @@
 //- (void)dch_setWebHighlightedImageLoadOperation:(id)operation;
 //- (void)dch_cancelCurrentWebHighlightedImageLoadOperation;
 //
-//#pragma mark - Local image
+#pragma mark - Local image
+- (NSMutableDictionary *)dch_localImagePathStorage;
+- (NSURL *)dch_currentLocalImageURL;
+- (NSURL *)dch_localImageURLForState:(UIControlState)state;
+
+- (NSMutableDictionary *)dch_localBackgroundImagePathStorage;
+- (NSURL *)dch_currentLocalBackgroundImageURL;
+- (NSURL *)dch_localBackgroundImageURLForState:(UIControlState)state;
+
+- (void)dch_setLocalImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state;
+- (void)dch_cancelLocalImageURLForState:(UIControlState)state;
+
+- (void)dch_setLocalBackgroundImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state;
+- (void)dch_cancelLocalBackgroundImageURLForState:(UIControlState)state;
 //- (NSString *)dch_currentLocalImagePath;
 //- (void)dch_setLocalImagePath:(NSString *)path;
 //
--- DCHImageTurbo/UIButton+DCHImageTurbo.m
+++ DCHImageTurbo/UIButton+DCHImageTurbo.m
@@ -7,7 +7,177 @@
 //
 
 #import "UIButton+DCHImageTurbo.h"
+#import "UIView+DCHImageTurbo.h"
+#import <libextobjc/EXTScope.h>
+#import "UIImage+DCHImageTurbo.h"
+
+NSString * const key_DCHImageTurbo_UIButton_WebImageURLStorage = @"key_DCHImageTurbo_UIButton_WebImageURLStorage";
+NSString * const key_DCHImageTurbo_UIButton_WebBackgroundImageURLStorage = @"key_DCHImageTurbo_UIButton_WebBackgroundImageURLStorage";
+NSString * const key_DCHImageTurbo_UIButton_LocalImagePathStorage = @"key_DCHImageTurbo_UIButton_LocalImagePathStorage";
+NSString * const key_DCHImageTurbo_UIButton_LocalBackgroundImagePathStorage = @"key_DCHImageTurbo_UIButton_LocalBackgroundImagePathStorage";
+
+NSString * const key_DCHImageTurbo_UIButton_WebImageLoadOperation = @"key_DCHImageTurbo_UIButton_WebImageLoadOperation";
+NSString * const key_DCHImageTurbo_UIButton_WebBackgroundImageLoadOperation = @"key_DCHImageTurbo_UIButton_WebBackgroundImageLoadOperation";
+NSString * const key_DCHImageTurbo_UIButton_LocalImageLoadOperation = @"key_DCHImageTurbo_UIButton_LocalImageLoadOperation";
+NSString * const key_DCHImageTurbo_UIButton_LocalBackgroundImageLoadOperation = @"key_DCHImageTurbo_UIButton_LocalBackgroundImageLoadOperation";
 
 @implementation UIButton (DCHImageTurbo)
+
+- (NSString *)dch_createKeyForButtonLoadImageWithPrefix:(NSString *)prefix andState:(UIControlState)state {
+    NSString *result = nil;
+    do {
+        if (DCH_IsEmpty(prefix)) {
+            break;
+        }
+        result = [NSString stringWithFormat:@"%@%@", prefix, @(state)];
+    } while (NO);
+    return result;
+}
+
+#pragma mark - Web image
+- (NSMutableDictionary *)dch_webImageURLStorage {
+    NSMutableDictionary *storage = [[self getImageLocationStorage] dch_safe_objectForKey:key_DCHImageTurbo_UIButton_WebImageURLStorage];
+    if (!storage) {
+        storage = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, &key_DCHImageTurbo_UIButton_WebImageURLStorage, storage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return storage;
+}
+
+- (NSURL *)dch_currentWebImageURL {
+    NSURL *url = [self dch_webImageURLStorage][@(self.state)];
+    
+    if (!url) {
+        url = [self dch_webImageURLStorage][@(UIControlStateNormal)];
+    }
+    
+    return url;
+}
+
+- (NSURL *)dch_webImageURLForState:(UIControlState)state {
+    return [self dch_webImageURLStorage][@(state)];
+}
+
+- (NSMutableDictionary *)dch_webBackgroundImageURLStorage {
+    NSMutableDictionary *storage = [[self getImageLocationStorage] dch_safe_objectForKey:key_DCHImageTurbo_UIButton_WebBackgroundImageURLStorage];
+    if (!storage) {
+        storage = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, &key_DCHImageTurbo_UIButton_WebBackgroundImageURLStorage, storage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return storage;
+}
+
+- (NSURL *)dch_currentWebBackgroundImageURL {
+    NSURL *url = [self dch_webImageURLStorage][@(self.state)];
+    
+    if (!url) {
+        url = [self dch_webImageURLStorage][@(UIControlStateNormal)];
+    }
+    
+    return url;
+}
+
+- (NSURL *)dch_webBackgroundImageURLForState:(UIControlState)state {
+    return [self dch_webImageURLStorage][@(state)];
+}
+
+- (void)dch_setWebImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state {
+    do {
+        if (DCH_IsEmpty(operation)) {
+            break;
+        }
+        [self sd_setImageLoadOperation:operation forKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_WebImageLoadOperation andState:state]];
+    } while (NO);
+}
+
+- (void)dch_cancelWebImageURLForState:(UIControlState)state {
+    [self sd_cancelImageLoadOperationWithKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_WebImageLoadOperation andState:state]];
+}
+
+- (void)dch_setWebBackgroundImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state {
+    do {
+        if (DCH_IsEmpty(operation)) {
+            break;
+        }
+        [self sd_setImageLoadOperation:operation forKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_WebBackgroundImageLoadOperation andState:state]];
+    } while (NO);
+}
+
+- (void)dch_cancelWebBackgroundImageURLForState:(UIControlState)state {
+    [self sd_cancelImageLoadOperationWithKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_WebBackgroundImageLoadOperation andState:state]];
+}
+
+#pragma mark - Local image
+- (NSMutableDictionary *)dch_localImagePathStorage {
+    NSMutableDictionary *storage = [[self getImageLocationStorage] dch_safe_objectForKey:key_DCHImageTurbo_UIButton_LocalImagePathStorage];
+    if (!storage) {
+        storage = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, &key_DCHImageTurbo_UIButton_LocalImagePathStorage, storage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return storage;
+}
+
+- (NSURL *)dch_currentLocalImageURL {
+    NSURL *url = [self dch_localImagePathStorage][@(self.state)];
+    
+    if (!url) {
+        url = [self dch_localImagePathStorage][@(UIControlStateNormal)];
+    }
+    
+    return url;
+}
+
+- (NSURL *)dch_localImageURLForState:(UIControlState)state {
+    return [self dch_localImagePathStorage][@(state)];
+}
+
+- (NSMutableDictionary *)dch_localBackgroundImagePathStorage {
+    NSMutableDictionary *storage = [[self getImageLocationStorage] dch_safe_objectForKey:key_DCHImageTurbo_UIButton_LocalBackgroundImagePathStorage];
+    if (!storage) {
+        storage = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, &key_DCHImageTurbo_UIButton_LocalBackgroundImagePathStorage, storage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return storage;
+}
+
+- (NSURL *)dch_currentLocalBackgroundImageURL {
+    NSURL *url = [self dch_localImagePathStorage][@(self.state)];
+    
+    if (!url) {
+        url = [self dch_localImagePathStorage][@(UIControlStateNormal)];
+    }
+    
+    return url;
+}
+
+- (NSURL *)dch_localBackgroundImageURLForState:(UIControlState)state {
+    return [self dch_localImagePathStorage][@(state)];
+}
+
+- (void)dch_setLocalImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state {
+    do {
+        if (DCH_IsEmpty(operation)) {
+            break;
+        }
+        [self sd_setImageLoadOperation:operation forKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_LocalImageLoadOperation andState:state]];
+    } while (NO);
+}
+
+- (void)dch_cancelLocalImageURLForState:(UIControlState)state {
+    [self sd_cancelImageLoadOperationWithKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_LocalImageLoadOperation andState:state]];
+}
+
+- (void)dch_setLocalBackgroundImageLoadOperation:(id <SDWebImageOperation>)operation forState:(UIControlState)state {
+    do {
+        if (DCH_IsEmpty(operation)) {
+            break;
+        }
+        [self sd_setImageLoadOperation:operation forKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_LocalBackgroundImageLoadOperation andState:state]];
+    } while (NO);
+}
+
+- (void)dch_cancelLocalBackgroundImageURLForState:(UIControlState)state {
+    [self sd_cancelImageLoadOperationWithKey:[self dch_createKeyForButtonLoadImageWithPrefix:key_DCHImageTurbo_UIButton_LocalBackgroundImageLoadOperation andState:state]];
+}
 
 @end
